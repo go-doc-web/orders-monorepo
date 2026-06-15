53e5432f37adb620a2180a6712439eae30abdbe6
Oleh Hubskiy <gubskyolegod@gmail.com>
2026-06-15 19:40:49 +0300

    feat(back):integrate Socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Application } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 
 // Routes import
 import ordersRouter from "./routes/orders.js";
 import productsRouter from "./routes/products.js";
+import { initCounterSocket } from "./sockets/counter.js";
 
 const PORT: number = Number(process.env.PORT) || 3001;
 
 const app: Application = express();
+const httpServer = createServer(app);
+
+// Настраиваем инстанс Socket.io («пульт управления сокетами»)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // Разрешаем временное подключение со всех хостов при разработке
+    methods: ["GET", "POST", "DELETE"],
+  },
+});
 // Midlevare
 app.use(cors());
 app.use(express.json());
@@ -16,8 +28,12 @@ app.use(express.json());
 app.use("/api/orders", ordersRouter);
 app.use("/api/products", productsRouter);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Тестовый сервер успешно запущен на порту ${PORT}`);
+initCounterSocket(io);
+
+httpServer.listen(PORT, () => {
+  console.log(
+    `🚀 Модульный сервер Express + WebSockets запущен на порту ${PORT}`,
+  );
   console.log(`🔗 Проверить приходы: http://localhost:${PORT}/api/orders`);
   console.log(`🔗 Проверить продукты: http://localhost:${PORT}/api/products`);
 });
