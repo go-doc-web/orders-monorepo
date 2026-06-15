87de9337f7d6cd728da3d28e41f896f4932b72b0
Oleh Hubskiy <gubskyolegod@gmail.com>
2026-06-15 19:30:44 +0300

    feat(back):Rest Api routes for products

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -1,9 +1,28 @@
 import { Router, Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
 
 const router = Router();
+// Get : получить плоский список всех продуктов
 
-router.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Тут будут продукты" });
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        order: {
+          select: {
+            title: true,
+          },
+        },
+      },
+      orderBy: {
+        id: "asc",
+      },
+    });
+    res.json(products);
+  } catch (error) {
+    console.error("Ошибка API Products:", error);
+    res.status(500).json({ error: "Ошибка сервера при получении продуктов" });
+  }
 });
 
 export default router;
