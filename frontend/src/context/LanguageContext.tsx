03b4e612c08bbb870416ef65922a07aef9a9181f
Oleh Hubskiy <gubskyolegod@gmail.com>
2026-06-16 15:04:23 +0300

    feat(f): implement custom i18 using ReactContext and Web Storage

---
+++ frontend/src/context/LanguageContext.tsx
@@ -0,0 +1,49 @@
+"use client";
+//TODO решить вопрос с гидрацией и каскаднім рендером
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { translations, LocaleType } from "../locales/translations";
+
+type LanguageContextType = {
+  locale: LocaleType;
+  t: (typeof translations)["uk"];
+  changeLanguage: (lang: LocaleType) => void;
+};
+
+const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined,
+);
+
+export const LanguageProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [locale, setLocale] = useState<LocaleType>("uk");
+
+  const changeLanguage = (lang: LocaleType) => {
+    setLocale(lang);
+    localStorage.setItem("app_locale", lang);
+  };
+
+  useEffect(() => {
+    const saved = localStorage.getItem("app_locale") as LocaleType;
+    if (saved && (saved === "uk" || saved === "en" || saved === "ru")) {
+      setLocale(saved);
+    }
+  }, []);
+
+  return (
+    <LanguageContext.Provider
+      value={{ locale, t: translations[locale], changeLanguage }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useTranslation = () => {
+  const context = useContext(LanguageContext);
+  if (!context)
+    throw new Error("useTranslation must be used within LanguageProvider");
+  return context;
+};
---
+++ frontend/src/locales/translations.ts
@@ -0,0 +1,43 @@
+export type LocaleType = "uk" | "en" | "ru";
+
+export const translations = {
+  uk: {
+    title: "Система Управління Інвентарем",
+    menu: {
+      orders: "ПРИХІД",
+      groups: "ГРУПИ",
+      products: "ПРОДУКТИ",
+      users: "КОРИСТУВАЧІ",
+      settings: "НАЛАШТУВАННЯ",
+    },
+    topMenu: {
+      activeSessions: "Активних сесій",
+    },
+  },
+  en: {
+    title: "Inventory Management System",
+    menu: {
+      orders: "ORDERS",
+      groups: "GROUPS",
+      products: "PRODUCTS",
+      users: "USERS",
+      settings: "SETTINGS",
+    },
+    topMenu: {
+      activeSessions: "Active sessions",
+    },
+  },
+  ru: {
+    title: "Система Управления Инвентарем",
+    menu: {
+      orders: "ПРИХОД",
+      groups: "ГРУППЫ",
+      products: "ПРОДУКТЫ",
+      users: "ПОЛЬЗОВАТЕЛИ",
+      settings: "НАСТРОЙКИ",
+    },
+    topMenu: {
+      activeSessions: "Активных сессий",
+    },
+  },
+};
